24815ee70315fcbf3d826a77d9920ec29e66c95a
刘帅 <liushuai@liushuaideMacBook-Pro.local>
2017-05-15 11:52:20 +0800

    创建test工程

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 刘帅 on 2017/5/15.
+//  Copyright © 2017年 刘帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by 刘帅 on 2017/5/15.
+//  Copyright © 2017年 刘帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
